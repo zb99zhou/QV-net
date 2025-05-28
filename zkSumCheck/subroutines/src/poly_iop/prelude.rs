205d4b62b1aba52d97b4585fb2808b160d9ca5ff
ZhBwen <474095007@qq.com>
2025-05-28 21:19:52 +0800

    removed useless dependencies in hyperplonk

---
+++ zkSumCheck/subroutines/src/poly_iop/prelude.rs
@@ -0,0 +1,12 @@
+// Copyright (c) 2023 Espresso Systems (espressosys.com)
+// This file is part of the HyperPlonk library.
+
+// You should have received a copy of the MIT License
+// along with the HyperPlonk library. If not, see <https://mit-license.org/>.
+
+#![allow(unused_imports)]
+
+pub use crate::poly_iop::{
+    errors::PolyIOPErrors, structs::IOPProof, PolyIOP,
+    zk_sum_check::ZkSumCheck, structs::RandomMaskPolynomial
+};
